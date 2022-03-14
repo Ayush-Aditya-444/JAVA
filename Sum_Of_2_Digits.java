8d95223367a51c6a03eb607d863f10eb119252a0
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-03-14 08:12:34 +0530

    Add files via upload

---
+++ Sum_Of_2_Digits.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Sum_Of_2_Digits {
+    public static void main(String[] args) {
+        pratt();
+    }
+    public static void pratt(){
+        int a=1;
+        int b=2;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
