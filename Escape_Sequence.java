94d3bfe73a06315dd72be05c51b1b670f93b82dc
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-03-16 00:12:07 +0530

    Add files via upload

---
+++ Escape_Sequence.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Escape_Sequence {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello \"World");
+        System.out.println("Hello \\World");
+        System.out.println("Hello \nWorld");
+        System.out.println("Hello \tWorld");
+    }
+}
