8314cad0c0e474c3cff28348fc313b1db0d4d974
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-03-15 17:36:35 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age=30;
+        int myAge=age;
+        System.out.println(myAge);
+        System.out.println(age);
+    }
+}
