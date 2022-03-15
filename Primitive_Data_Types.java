b80baf3c660355349c34db423d8c219ffa94eb72
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-03-15 17:36:15 +0530

    Add files via upload

---
+++ Primitive_Data_Types.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Primitive_Data_Types {
+
+    public static void main(String[] args) {
+        // write your code here
+        int age=30;
+        byte a=12;
+        long b=3_000_000_000_000L;
+        float c=4.565F;
+        double d=3.45467545;
+        char e='A';
+        boolean f=true;
+        System.out.println("Integer Value = "+age);
+        System.out.println("Byte = "+a);
+        System.out.println("Long Integer = "+b);
+        System.out.println("Float = "+c);
+        System.out.println("Double = "+d);
+        System.out.println("Character = "+e);
+        System.out.println("Boolean = "+f);
+    }
+}
