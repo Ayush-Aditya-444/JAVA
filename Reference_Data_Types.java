d957ab151a79d5b3237072ce64f77109c046a2b0
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-03-15 17:36:25 +0530

    Add files via upload

---
+++ Reference_Data_Types.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Date;
+
+public class Reference_Data_Types {
+
+    public static void main(String[] args){
+        int age=30;
+        Date now=new Date();
+        now.getTime();
+        now.clone();
+        System.out.println(now);
+    }
+}
