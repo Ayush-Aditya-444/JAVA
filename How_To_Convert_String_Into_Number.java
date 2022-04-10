d9c589026a0b971ab19a0a70a3154da77942b567
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-10 08:11:38 +0530

    Add files via upload

---
+++ How_To_Convert_String_Into_Number.java
@@ -0,0 +1,10 @@
+public class How_To_Convert_String_Into_Number {
+    public static void main(String[] args) {
+        int a = 1;
+        String b="6", d="7.7";
+        int c=Integer.parseInt(b)+a;
+        double e=Double.parseDouble(d)+a;
+        System.out.println(c);
+        System.out.println(e);
+    }
+}
