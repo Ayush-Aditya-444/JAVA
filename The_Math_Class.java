63acd3ce82f9e792704f160179c3bd5e9b56bfcc
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-10 08:12:05 +0530

    Add files via upload

---
+++ The_Math_Class.java
@@ -0,0 +1,28 @@
+public class The_Math_Class {
+    public static void main(String[] args) {
+        int a = Math.round(5.9F);
+        int b = (int)Math.ceil(6.6F);
+        int c = (int)Math.floor(6.6F);
+        int d = (int)Math.max(6, 7);
+        int e = (int)Math.min(9, 8);
+        int f = (int)Math.abs(8.8);
+        int g = (int)Math.sqrt(9);
+        int h = (int)Math.pow(2, 3);
+        double i = Math.random() * 100;
+        int j = (int)Math.round(Math.random()*100);
+        int k = (int)(Math.random()*100);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+
+    }
+    
+}
