f056103a6483158b79b086a847fc7f3d34ceb913
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-10 08:11:25 +0530

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,14 @@
+public class Casting {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        double c=1.1;
+        int d=a+(int)c;
+        String e="6";
+        int f=Integer.parseInt(e)+a;
+        System.out.println(a+b);
+        System.out.println(a+c);
+        System.out.println(d);
+        System.out.println(f);
+
+    }
+}
