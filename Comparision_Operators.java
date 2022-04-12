2844e611c4ac758b5de13cd061e3a31de2d256e8
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-12 09:21:53 +0530

    Add files via upload

---
+++ Comparision_Operators.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Comparision_Operators {
+    private static Scanner a;
+    public static void main(String[] args) {
+        int x = 1, y = 1;
+        a = new Scanner(System.in);
+        int c = a.nextInt();
+        int d = a.nextInt();
+        System.out.println(x==y);
+        System.out.println(x!=y);
+        System.out.println(c);
+        System.out.println(d);
+    }
+    
+}
