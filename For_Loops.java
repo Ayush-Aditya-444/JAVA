6da7fed41aad5ebb14906cc3126a95bca07ed6ca
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-16 08:57:44 +0530

    Add files via upload

---
+++ For_Loops.java
@@ -0,0 +1,17 @@
+public class For_Loops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World");
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World " +i);
+        }
+        for (int i = 5; i > 0; i--) {
+            System.out.println("Hello World " +i);
+        }
+    }
+}
