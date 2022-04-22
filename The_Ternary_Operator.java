802a41826e7618cf445949cdec449850d21ac8f6
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-22 09:17:35 +0530

    Add files via upload

---
+++ The_Ternary_Operator.java
@@ -0,0 +1,7 @@
+public class The_Ternary_Operator {
+    public static void main(String[] args) {
+        int income = 120_000;
+        String className = income > 100_000 ? "First Class" : "Economy";
+        System.out.println(className);
+    }
+}
