fda1a95ea79ae30163746fcb5339f62118e48d70
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-23 08:56:56 +0530

    Add files via upload

---
+++ Simplifying_If_Statement.java
@@ -0,0 +1,7 @@
+public class Simplifying_If_Statement {
+    public static void main(String[] args) {
+        int income = 120_000;
+        boolean HasHighIncome = income > 100_000;
+        System.out.println(HasHighIncome);
+    }
+}
